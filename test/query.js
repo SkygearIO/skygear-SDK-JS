4f9a41c4a3a1b21cdb02d1dfde3708d23a19185d
Rick Mak <rick.mak@gmail.com>
2015-09-11 11:45:44 +0800

    Implement query with predicate, sort, limit and offset
    refs #2

--- test/query.js
+++ test/query.js
@@ -31,49 +31,79 @@ describe('Query', function () {
     expect(q.recordType).to.be.equal('note');
   });
 
+  it('default return null predicate', function () {
+    let q = new Query(Note);
+    expect(q.predicate).to.eql([]);
+  });
+
   it('add equal to predicate', function () {
     let q = new Query(Note);
     q.equalTo('content', 'First one');
-    expect(q.predicate).to.deep.include.members([['eq', 'content', 'First one']]);
+    expect(q.predicate).to.deep.include.members(['eq', {
+      $type: 'keypath',
+      $val: 'content'
+    }, 'First one']);
   });
 
   it('add not equal to predicate', function () {
     let q = new Query(Note);
     q.notEqualTo('content', 'First one');
-    expect(q.predicate).to.deep.include.members([['neq', 'content', 'First one']]);
+    expect(q.predicate).to.deep.include.members(['neq', {
+      $type: 'keypath',
+      $val: 'content'
+    }, 'First one']);
   });
 
   it('add greater than to predicate', function () {
     let q = new Query(Note);
     q.greaterThan('price', 10);
-    expect(q.predicate).to.deep.include.members([['gt', 'price', 10]]);
+    expect(q.predicate).to.deep.include.members(['gt', {
+      $type: 'keypath',
+      $val: 'price'
+    }, 10]);
   });
 
   it('add greater than or equal to predicate', function () {
     let q = new Query(Note);
     q.greaterThanOrEqualTo('price', 10);
-    expect(q.predicate).to.deep.include.members([['gte', 'price', 10]]);
+    expect(q.predicate).to.deep.include.members(['gte', {
+      $type: 'keypath',
+      $val: 'price'
+    }, 10]);
   });
 
   it('add less than to predicate', function () {
     let q = new Query(Note);
     q.lessThan('price', 10);
-    expect(q.predicate).to.deep.include.members([['lt', 'price', 10]]);
+    expect(q.predicate).to.deep.include.members(['lt', {
+      $type: 'keypath',
+      $val: 'price'
+    }, 10]);
   });
 
   it('add less than to predicate', function () {
     let q = new Query(Note);
     q.lessThanOrEqualTo('price', 10);
-    expect(q.predicate).to.deep.include.members([['lte', 'price', 10]]);
+    expect(q.predicate).to.deep.include.members(['lte', {
+      $type: 'keypath',
+      $val: 'price'
+    }, 10]);
   });
 
-  it('have multiple predicate', function () {
+  it('have multiple predicate with and', function () {
     let q = new Query(Note);
     q.equalTo('content', 'food');
     q.greaterThan('price', 10);
     expect(q.predicate).to.deep.include.members([
-      ['gt', 'price', 10],
-      ['eq', 'content', 'food']
+      'and',
+      ['gt', {
+        $type: 'keypath',
+        $val: 'price'
+      }, 10],
+      ['eq', {
+        $type: 'keypath',
+        $val: 'content'
+      }, 'food']
     ]);
   });
 
@@ -82,9 +112,37 @@ describe('Query', function () {
     q.lessThanOrEqualTo('price', 100);
     q.greaterThan('price', 0);
     expect(q.predicate).to.deep.include.members([
-      ['gt', 'price', 0],
-      ['lte', 'price', 100]
+      'and',
+      ['gt', {
+        $type: 'keypath',
+        $val: 'price'
+      }, 0],
+      ['lte', {
+        $type: 'keypath',
+        $val: 'price'
+      }, 100]
     ]);
   });
+
+  it('serialize a simple query payload', function () {
+    let q = new Query(Note);
+    q.like('content', 'hello');
+    q.limit = 10;
+    q.offset = 5;
+    q.addDescending('noteOrder');
+    expect(q.toJSON()).to.eql({
+      record_type: 'note',
+      limit: 10,
+      offset: 5,
+      sort: [[{
+        $type: 'keypath',
+        $val: 'noteOrder'
+      }, 'desc']],
+      predicate: ['like', {
+        $type: 'keypath',
+        $val: 'content'
+      }, 'hello']
+    })
+  })
 
 });
