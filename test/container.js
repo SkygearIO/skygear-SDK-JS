622237f201c17d4804f9ccd0fdd977be1efccaf6
Ben Lei <benlei@oursky.com>
2016-12-16 17:16:06 +0800

    Support unregister device

--- test/container.js
+++ test/container.js
@@ -861,6 +861,81 @@ describe('Container device registration', function () {
   });
 });
 
+describe('Container device unregistration', function () {
+  let container = new Container();
+  container.autoPubsub = false;
+  container.configApiKey('correctApiKey');
+  container.request = mockSuperagent([{
+    pattern: 'http://skygear.dev/device/unregister',
+    fixtures: function (match, params, headers, fn) {
+      if (params.id && params.id === 'non-exist') {
+        return fn({
+          'error': {
+            'name': 'ResourceNotFound',
+            'code': 110,
+            'message': 'device not found'
+          }
+        }, 400);
+      } else if (params.id) {
+        return fn({
+          'result': {
+            'id': params.id
+          }
+        });
+      } else {
+        return fn({
+          'error': {
+            'name': 'InvalidArgument',
+            'code': 108,
+            'message': 'Missing device id',
+            'info': {
+              'arguments': [
+                'id'
+              ]
+            }
+          }
+        });
+      }
+    }
+  }]);
+
+  it('should success with correct device id', function (done) {
+    return container._setDeviceID('device_1')
+    .then(function () {
+      return container.unregisterDevice();
+    })
+    .then(function () {
+      done();
+    }, function () {
+      throw new Error('Should not fail with correct device id');
+    });
+  });
+
+  it('should regard as success with non-exist device id', function (done) {
+    return container._setDeviceID('non-exist')
+    .then(function () {
+      return container.unregisterDevice();
+    })
+    .then(function () {
+      done();
+    }, function () {
+      throw new Error('Should not fail with non-exist device id');
+    });
+  });
+
+  it('should fail when no device id', function (done) {
+    return container._setDeviceID(null)
+    .then(function () {
+      return container.unregisterDevice();
+    })
+    .then(function () {
+      throw new Error('Should not success without device id');
+    }, function () {
+      done();
+    });
+  });
+});
+
 describe('lambda', function () {
   let container = new Container();
   container.autoPubsub = false;
