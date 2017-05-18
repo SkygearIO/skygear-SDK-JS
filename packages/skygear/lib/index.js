1d4c293de93a1355ef18eaf0f502f2e35eb93041
Brian Chan <i@b123400.net>
2017-05-18 11:50:51 +0100

    Inject functions into prototype so all instance get the method

--- packages/skygear/lib/index.js
+++ packages/skygear/lib/index.js
@@ -1,6 +1,6 @@
-module.exports = require('skygear-core');
-
+const container = require('skygear-core');
 const forgotPassword = require('skygear-forgot-password');
-for (let key in forgotPassword) {
-  module.exports[key] = forgotPassword[key];
-}
+
+forgotPassword.injectToContainer(container);
+
+module.exports = container;
