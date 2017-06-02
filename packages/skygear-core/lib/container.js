13ba63fe19079036c441b23390d8777b9dab483e
stevenchan <stevenchan@oursky.com>
2017-06-02 19:57:54 +0800

    Implement BaseContainer, extend BaseContainer to Container
    Keep basic app state in BaseContainer, while children containers are
    injected into Container with some extended behaviour.

--- packages/skygear-core/lib/container.js
+++ packages/skygear-core/lib/container.js
@@ -37,53 +37,13 @@ import {DatabaseContainer} from './database';
 import {PubsubContainer} from './pubsub';
 import {PushContainer} from './push';
 
-export default class Container {
+export class BaseContainer {
 
   constructor() {
     this.url = '/* @echo API_URL */';
     this.apiKey = null;
     this.request = request;
     this.ee = ee({});
-
-    this._auth = new AuthContainer(this);
-    this._relation = new RelationContainer(this);
-    this._db = new DatabaseContainer(this);
-    this._pubsub = new PubsubContainer(this);
-    this._push = new PushContainer(this);
-    /**
-     * Options for how much time to wait for client request to complete.
-     *
-     * @type {Object}
-     * @property {number} [timeoutOptions.deadline] - deadline for the request
-     * and response to complete (in milliseconds)
-     * @property {number} [timeoutOptions.response=60000] - maximum time to
-     * wait for an response (in milliseconds)
-     *
-     * @see http://visionmedia.github.io/superagent/#timeouts
-     */
-    this.timeoutOptions = {
-      response: 60000
-    };
-  }
-
-  get auth() {
-    return this._auth;
-  }
-
-  get relation() {
-    return this._relation;
-  }
-
-  get db() {
-    return this._db;
-  }
-
-  get pubsub() {
-    return this._pubsub;
-  }
-
-  get push() {
-    return this._push;
   }
 
   config(options) {
@@ -94,31 +54,25 @@ export default class Container {
       this.endPoint = options.endPoint;
     }
 
-    let promises = [
-      this.auth._getUser(),
-      this.auth._getAccessToken(),
-      this.push._getDeviceID()
-    ];
-    return Promise.all(promises).then(()=> {
-      this.pubsub._reconfigurePubsubIfNeeded();
-      return this;
-    }, ()=> {
-      return this;
-    });
+    return Promise.resolve(this);
   }
 
   configApiKey(ApiKey) {
     this.apiKey = ApiKey;
   }
 
-  clearCache() {
-    return this.store.clearPurgeableItems();
-  }
+  makeRequest(action, data) {
+    let requestObject = this._prepareRequestObject(action, data);
+    let requestData = this._prepareRequestData(action, data);
 
-  lambda(name, data) {
-    return this.makeRequest(name, {
-      args: data
-    }).then((resp)=> resp.result);
+    return this._handleResponse(new Promise((resolve)=> {
+      requestObject.send(requestData).end((err, res)=> {
+        resolve({
+          err: err,
+          res: res,
+        });
+      });
+    }));
   }
 
   makeUploadAssetRequest(asset) {
@@ -163,57 +117,51 @@ export default class Container {
     });
   }
 
-  sendRequestObject(action, data) {
+  lambda(name, data) {
+    return this.makeRequest(name, {
+      args: data
+    }).then((resp)=> resp.result);
+  }
+
+  _prepareRequestObject(action, data) {
     if (this.apiKey === null) {
       throw Error('Please config ApiKey');
     }
-    let _data = _.assign({
-      action: action,
-      api_key: this.apiKey,
-      access_token: this.auth.accessToken
-    }, data);
+
     let _action = action.replace(/:/g, '/');
-    let req = this.request
+    return this.request
       .post(this.url + _action)
       .set('X-Skygear-API-Key', this.apiKey)
-      .set('X-Skygear-Access-Token', this.auth.accessToken)
       .set('Accept', 'application/json');
-    if (this.timeoutOptions !== undefined && this.timeoutOptions !== null) {
-      req = req.timeout(this.timeoutOptions);
-    }
-    return req.send(_data);
   }
 
-  makeRequest(action, data) {
-    let _request = this.sendRequestObject(action, data);
-    return new Promise((resolve, reject)=> {
-      _request.end((err, res)=> {
-        // Do an application JSON parse because in some condition, the
-        // content-type header will got strip and it will not deserial
-        // the json for us.
-        let body = getRespJSON(res);
+  _prepareRequestData(action, data) {
+    if (this.apiKey === null) {
+      throw Error('Please config ApiKey');
+    }
 
-        if (err) {
-          let skyErr = body.error || err;
-          if (skyErr.code === this.ErrorCodes.AccessTokenNotAccepted) {
-            return Promise.all([
-              this.auth._setAccessToken(null),
-              this.auth._setUser(null)
-            ]).then(function () {
-              reject({
-                status: err.status,
-                error: skyErr
-              });
-            });
-          }
-          reject({
-            status: err.status,
-            error: skyErr
-          });
-        } else {
-          resolve(body);
-        }
-      });
+    return _.assign({
+      action: action,
+      api_key: this.apiKey,
+    }, data);
+  }
+
+  _handleResponse(responsePromise) {
+    return responsePromise.then(({err, res})=> {
+      // Do an application JSON parse because in some condition, the
+      // content-type header will got strip and it will not deserial
+      // the json for us.
+      let body = getRespJSON(res);
+
+      if (err) {
+        let skyErr = body.error || err;
+        return Promise.reject({
+          status: err.status,
+          error: skyErr
+        });
+      } else {
+        return Promise.resolve(body);
+      }
     });
   }
 
@@ -282,8 +230,115 @@ export default class Container {
     return this._store;
   }
 
+  clearCache() {
+    return this.store.clearPurgeableItems();
+  }
+
   get Database() {
     return Database;
+  }
+
+}
+
+export default class Container extends BaseContainer {
+
+  constructor() {
+    super();
+
+    this._auth = new AuthContainer(this);
+    this._relation = new RelationContainer(this);
+    this._db = new DatabaseContainer(this);
+    this._pubsub = new PubsubContainer(this);
+    this._push = new PushContainer(this);
+    /**
+     * Options for how much time to wait for client request to complete.
+     *
+     * @type {Object}
+     * @property {number} [timeoutOptions.deadline] - deadline for the request
+     * and response to complete (in milliseconds)
+     * @property {number} [timeoutOptions.response=60000] - maximum time to
+     * wait for an response (in milliseconds)
+     *
+     * @see http://visionmedia.github.io/superagent/#timeouts
+     */
+    this.timeoutOptions = {
+      response: 60000
+    };
+  }
+
+  get auth() {
+    return this._auth;
+  }
+
+  get relation() {
+    return this._relation;
+  }
+
+  get db() {
+    return this._db;
+  }
+
+  get pubsub() {
+    return this._pubsub;
+  }
+
+  get push() {
+    return this._push;
+  }
+
+  config(options) {
+    return super.config(options).then(()=> {
+      let promises = [
+        this.auth._getUser(),
+        this.auth._getAccessToken(),
+        this.push._getDeviceID()
+      ];
+      return Promise.all(promises);
+    }).then(()=> {
+      this.pubsub._reconfigurePubsubIfNeeded();
+      return this;
+    }, ()=> {
+      return this;
+    });
+  }
+
+  _prepareRequestObject(action, data) {
+    let requestObject = super._prepareRequestObject(action, data);
+
+    requestObject = requestObject
+      .set('X-Skygear-Access-Token', this.auth.accessToken);
+
+    if (this.timeoutOptions !== undefined && this.timeoutOptions !== null) {
+      requestObject = requestObject.timeout(this.timeoutOptions);
+    }
+
+    return requestObject;
+  }
+
+  _prepareRequestData(action, data) {
+    let requestData = super._prepareRequestData(action, data);
+
+    return _.assign({
+      access_token: this.auth.accessToken
+    }, requestData);
+  }
+
+  _handleResponse(responsePromise) {
+    return super._handleResponse(responsePromise)
+      .catch((err)=> {
+        // Logout user implicitly if
+        let errorCode = err.error.code;
+        if (errorCode === this.ErrorCodes.AccessTokenNotAccepted) {
+          return Promise.all([
+            this.auth._setAccessToken(null),
+            this.auth._setUser(null)
+          ]).then(() => {
+            return Promise.reject(err);
+          });
+        }
+
+        return Promise.reject(err);
+      });
   }
 
 }
