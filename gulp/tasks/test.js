19a6388e656a014d9c9dd272d40ee4520463998d
Kwok-kuen Cheung <cheungpat@oursky.com>
2017-06-05 17:40:08 +0800

    Fail test run if cannot load modules

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var plumber = require('gulp-plumber');
 var babel = require('gulp-babel');
 var preprocess = require('gulp-preprocess');
 var isparta = require('isparta');
@@ -34,21 +33,14 @@ gulp.task('pre-test', function () {
   return merge(streams);
 });
 
-gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
-
-  gulp.src(config.testSrc)
+gulp.task('test', ['pre-test'], function () {
+  return gulp.src(config.testSrc)
     .pipe(babel())
-    .pipe(plumber())
     .pipe(mocha({
       reporter: 'spec',
       clearRequireCache: true,
       quiet: false
     }))
-    .on('error', function (err) {
-      console.log(err);
-      mochaErr = err;
-    })
     .pipe(istanbul.writeReports({
       reporters: ['lcov', 'text', 'text-summary']
     }))
@@ -62,7 +54,4 @@ gulp.task('test', ['pre-test'], function (cb) {
         }
       }
     }))
-    .on('end', function () {
-      cb(mochaErr);
-    });
 });
