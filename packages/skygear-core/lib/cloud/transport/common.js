805645358dbc5095cfb4ff539ef4d9074e81ea1c
TY <tatgean@gmail.com>
2017-06-06 16:24:43 +0800

    Refactor code and rename functions

--- packages/skygear-core/lib/cloud/transport/common.js
+++ packages/skygear-core/lib/cloud/transport/common.js
@@ -407,7 +407,7 @@ export default class CommonTransport {
     if (!func) {
       return Promise.reject(new Error('Handler not exist'));
     }
-    param.params = this.registry.parseParamsInUrl(name);
+    param.params = this.registry.parseParamsInPath(name);
 
     const options = {
       context,
