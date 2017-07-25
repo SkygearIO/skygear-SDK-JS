74a2bc663ac6b2c999c0f8671d8beb39632b32f8
Rick Mak <rick.mak@gmail.com>
2017-07-25 15:22:10 +0800

    Fix exception on server fails to bind address and return meaningful message
    On server error, the callback only provide `err` and the socket is already
    closed. We should only just log out the error for user to inspection.

--- packages/skygear-core/lib/cloud/transport/http.js
+++ packages/skygear-core/lib/cloud/transport/http.js
@@ -41,8 +41,8 @@ class HTTPTransport extends CommonTransport {
     this.server.on('clientError', (err, socket) => {
       socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
     });
-    this.server.on('error', (err, socket) => {
-      socket.end('HTTP/1.1 500 Internal Server Error\r\n\r\n');
+    this.server.on('error', (err) => {
+      console.error(err);
     });
 
     const {
