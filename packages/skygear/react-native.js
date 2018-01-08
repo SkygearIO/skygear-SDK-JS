67702c7c1b21b74826f8d32e6359e3516dbeb599
Carmen <carmen@oursky.com>
2018-01-08 19:24:41 +0800

    Inject sso and forgot password to react native container

--- packages/skygear/react-native.js
+++ packages/skygear/react-native.js
@@ -1,1 +1,8 @@
-module.exports = require('skygear-core/dist/react-native/index.js');
+const container = require('skygear-core/dist/react-native/index.js');
+const forgotPassword = require('skygear-forgot-password');
+const sso = require('skygear-sso');
+
+forgotPassword.injectToContainer(container);
+sso.injectToContainer(container);
+
+module.exports = container;
