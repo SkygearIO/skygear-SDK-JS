ee423701b2fbdd40bec6e8255d15316f22ff0666
Kwok-kuen Cheung <cheungpat@oursky.com>
2018-01-10 14:58:55 +0800

    Add note on keeping injected modules in sync

--- packages/skygear/lib/index.js
+++ packages/skygear/lib/index.js
@@ -2,6 +2,8 @@ const container = require('skygear-core');
 const forgotPassword = require('skygear-forgot-password');
 const sso = require('skygear-sso');
 
+// Inject sub-package modules into container.
+// Should keep in-sync with (project-root)/lib/index.js.
 forgotPassword.injectToContainer(container);
 sso.injectToContainer(container);
 
--- packages/skygear/react-native.js
+++ packages/skygear/react-native.js
@@ -2,6 +2,8 @@ const container = require('skygear-core/dist/react-native/index.js');
 const forgotPassword = require('skygear-forgot-password');
 const sso = require('skygear-sso');
 
+// Inject sub-package modules into container.
+// Should keep in-sync with (project-root)/react-native.js.
 forgotPassword.injectToContainer(container);
 sso.injectToContainer(container);
 
