5fcd707da18873ce53e2a528b5307fa7404e494a
Kwok-kuen Cheung <cheungpat@oursky.com>
2018-07-11 16:33:19 +0800

    Update to babel 7

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    "presets": [
+      "@babel/env",
+      "@babel/react",
+      ["@babel/stage-2", {
+        "decoratorsLegacy": true
+      }]
+    ],
+    "plugins": [
+      "add-module-exports",
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-object-rest-spread",
+      "@babel/plugin-transform-classes",
+      ["@babel/plugin-transform-runtime", {
+        "polyfill": false,
+        "regenerator": true
+      }]
+    ]
+  };
+}
+
+
