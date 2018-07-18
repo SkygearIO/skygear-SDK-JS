f7398152c2518b2fb55c68c2471bb61a9d7aeeea
Steven Chan <stevenchan@oursky.com>
2018-07-18 12:10:27 +0800

    Fix try/catch syntax

--- packages/skygear-core/lib/database.js
+++ packages/skygear-core/lib/database.js
@@ -562,7 +562,7 @@ async function makeUploadAssetRequest(container, asset) {
         try {
           const [code, message] = await parseS3XmlErrorMessage(err);
           reject(_s3ErrorToSkyError(code, message));
-        } catch {
+        } catch (_err) {
           reject(err);
         }
 
