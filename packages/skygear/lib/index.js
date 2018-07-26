8cf40337fc6b778f6378fbd08dbefbf2b00750c1
Ben Lei <benlei@oursky.com>
2018-07-26 17:34:42 +0800

    Fix invalid esModule import in skygear package

--- packages/skygear/lib/index.js
+++ packages/skygear/lib/index.js
@@ -1,10 +1,10 @@
 import container from 'skygear-core';
-import forgotPassword from 'skygear-forgot-password';
-import sso from 'skygear-sso';
+import * as forgotPassword from 'skygear-forgot-password';
+import * as sso from 'skygear-sso';
 
 // Inject sub-package modules into container.
 // Should keep in-sync with (project-root)/lib/index.js.
 forgotPassword.injectToContainer(container);
 sso.injectToContainer(container);
 
-module.exports = container;
+export default container;
