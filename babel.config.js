80fdcccbc9f311579d45b0a9d83c3a5884634512
Louis Chan <louischan@oursky.com>
2019-07-24 16:29:54 +0800

    Bundle with inlined babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,66 @@
+var plugins = [
+  // TypeScript section begin
+  "@babel/plugin-transform-typescript",
+  "@babel/plugin-proposal-class-properties",
+  // TypeScript section end
+  // Inlined preset-env section begin
+  // The order is obtained by specifying debug: true
+  "@babel/plugin-transform-template-literals",
+  "@babel/plugin-transform-literals",
+  "@babel/plugin-transform-function-name",
+  "@babel/plugin-transform-arrow-functions",
+  "@babel/plugin-transform-block-scoped-functions",
+  "@babel/plugin-transform-classes",
+  "@babel/plugin-transform-object-super",
+  "@babel/plugin-transform-shorthand-properties",
+  "@babel/plugin-transform-duplicate-keys",
+  "@babel/plugin-transform-computed-properties",
+  [
+    "@babel/plugin-transform-for-of",
+    {
+      assumeArray: true,
+    },
+  ],
+  "@babel/plugin-transform-sticky-regex",
+  "@babel/plugin-transform-dotall-regex",
+  "@babel/plugin-transform-unicode-regex",
+  [
+    "@babel/plugin-transform-spread",
+    {
+      loose: true,
+    },
+  ],
+  "@babel/plugin-transform-parameters",
+  "@babel/plugin-transform-destructuring",
+  "@babel/plugin-transform-block-scoping",
+  "@babel/plugin-transform-typeof-symbol",
+  "@babel/plugin-transform-new-target",
+  "@babel/plugin-transform-regenerator",
+  "@babel/plugin-transform-exponentiation-operator",
+  "@babel/plugin-transform-async-to-generator",
+  "@babel/plugin-proposal-async-generator-functions",
+  "@babel/plugin-proposal-object-rest-spread",
+  "@babel/plugin-proposal-unicode-property-regex",
+  "@babel/plugin-proposal-json-strings",
+  "@babel/plugin-proposal-optional-catch-binding",
+  "@babel/plugin-transform-named-capturing-groups-regex",
+  "@babel/plugin-transform-member-expression-literals",
+  "@babel/plugin-transform-property-literals",
+  "@babel/plugin-transform-reserved-words",
+  // Inlined preset-env section end
+  // Use tranform-runtime to avoid polluting the global namespace.
+  [
+    "@babel/plugin-transform-runtime",
+    {
+      useESModules: true,
+    },
+  ],
+];
+
+if (process.env.NODE_ENV === "test") {
+  plugins.push("@babel/plugin-transform-modules-commonjs");
+}
+
 module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        // targets are not set because we want to apply all transforms
-        // targets: {},
-        //
-        // During test, transform modules so that Jest and Node can
-        // execute the code directly.
-        // In other cases, keep modules and let the bundler to
-        // deal with it.
-        modules: process.env.NODE_ENV === "test" ? "auto" : false,
-        // debug: true,
-      },
-    ],
-    "@babel/preset-typescript",
-  ],
-  plugins: [
-    // TODO: Remove class-properties once it becomes Stage 4
-    "@babel/plugin-proposal-class-properties",
-    [
-      "@babel/plugin-transform-runtime",
-      {
-        useESModules: true,
-      },
-    ],
-  ],
+  plugins,
 };
