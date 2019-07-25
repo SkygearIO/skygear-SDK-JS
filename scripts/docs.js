77d2909cef866b590eb9c30c8564f630fc8f9a0d
Louis Chan <louischan@oursky.com>
2019-07-25 19:39:06 +0800

    Generate documentation

---
+++ scripts/docs.js
@@ -0,0 +1,83 @@
+const fs = require("fs");
+const { exec } = require("child_process");
+const path = require("path");
+
+const dir = "./docs";
+
+function generateDocs() {
+  return new Promise((resolve, reject) => {
+    exec("api-documenter markdown -i temp -o docs", (err, stdout, stderr) => {
+      console.log(stdout);
+      console.error(stderr);
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function fixDocs() {
+  const filenames = fs.readdirSync(dir);
+  for (const filename of filenames) {
+    const { name: id, ext } = path.parse(filename);
+    if (ext !== ".md") {
+      continue;
+    }
+    const filepath = path.join(dir, filename);
+    const lines = fs.readFileSync(filepath, { encoding: "utf8" }).split("\r\n");
+
+    const output = [];
+    let title = "";
+    for (const line of lines) {
+      let skip = false;
+      if (!title) {
+        const titleLine = line.match(/## (.*)/);
+        if (titleLine) {
+          title = titleLine[1];
+        }
+      }
+      const homeLink = line.match(/\[Home\]\(.\/index\.md\) &gt; (.*)/);
+      if (homeLink) {
+        skip = true;
+      }
+      if (!skip) {
+        output.push(line);
+      }
+    }
+
+    const header = [
+      "---",
+      `id: ${id}`,
+      `title: ${title}`,
+      `hide_title: true`,
+      "---",
+    ];
+
+    fs.writeFileSync(filepath, header.concat(output).join("\n"));
+  }
+}
+
+function writeIndex() {
+  const lines = [
+    "---",
+    "id: index",
+    "title: Index",
+    "hide_title: true",
+    "---",
+    "",
+    "- [@skygear/web](./web.md)",
+    "- [@skygear/node](./node.md)",
+    "",
+  ];
+  fs.writeFileSync(path.join(dir, "./index.md"), lines.join("\n"));
+}
+
+async function main() {
+  await generateDocs();
+  fixDocs();
+  writeIndex();
+}
+
+main();
---
+++ website/siteConfig.js
@@ -0,0 +1,82 @@
+// See https://docusaurus.io/docs/site-config for all the possible
+// site configuration options.
+
+const siteConfig = {
+  title: "Skygear SDK JS",
+  tagline: "Documentation site for Skygear SDK JS",
+  // TODO: Fix URL
+  url: "https://your-docusaurus-test-site.com", // Your website URL
+  baseUrl: "/", // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: "skygear-sdk-js",
+  organizationName: "skygeario",
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [{ doc: "index", label: "API Reference" }],
+
+  // If you have users set above, you add it here:
+
+  // TODO: Update favicon
+  headerIcon: "img/favicon.ico",
+  footerIcon: "img/favicon.ico",
+  favicon: "img/favicon.ico",
+
+  /* Colors for website */
+  colors: {
+    primaryColor: "#115243",
+    secondaryColor: "#0b392e",
+  },
+
+  /* Custom fonts for website */
+  /*
+  fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },
+  */
+
+  // TODO: Fix copyright
+  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: "default",
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ["https://buttons.github.io/buttons.js"],
+
+  // On page navigation for the current documentation page.
+  onPageNav: "separate",
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // For sites with a sizable amount of content, set collapsible to true.
+  // Expand/collapse the links and subcategories under categories.
+  // docsSideNavCollapsible: true,
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
+};
+
+module.exports = siteConfig;
