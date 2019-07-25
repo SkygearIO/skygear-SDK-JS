d79c2323a229e29b0c41796d5bb66b25c36c9b68
Louis Chan <louischan@oursky.com>
2019-07-25 17:29:14 +0800

    Expose defaultContainer

--- packages/skygear-node/src/index.ts
+++ packages/skygear-node/src/index.ts
@@ -1,4 +1,4 @@
-import { BaseAPIClient } from "@skygear/core";
+import { BaseAPIClient, ContainerStorage, Container } from "@skygear/core";
 export * from "@skygear/core";
 
 const nodeFetch = require("node-fetch");
@@ -8,3 +8,35 @@ export class APIClient extends BaseAPIClient {
     return nodeFetch(input, init);
   }
 }
+
+export class MemoryStorage implements ContainerStorage {
+  backingStore: { [key: string]: string };
+
+  constructor() {
+    this.backingStore = {};
+  }
+
+  async get(key: string): Promise<string | null> {
+    const value = this.backingStore[key];
+    if (value != null) {
+      return value;
+    }
+    return null;
+  }
+  async set(key: string, value: string): Promise<void> {
+    this.backingStore[key] = value;
+  }
+  async del(key: string): Promise<void> {
+    delete this.backingStore[key];
+  }
+}
+
+export const defaultContainer: Container = new Container(
+  "default",
+  new APIClient({
+    apiKey: "",
+    endpoint: "",
+    accessToken: null,
+  }),
+  new MemoryStorage()
+);
--- packages/skygear-web/src/index.ts
+++ packages/skygear-web/src/index.ts
@@ -1,10 +1,33 @@
-import { BaseAPIClient } from "@skygear/core";
+import { BaseAPIClient, ContainerStorage, Container } from "@skygear/core";
 export * from "@skygear/core";
 
 const globalFetch = fetch;
+const globalLocalStorage = localStorage;
 
 export class APIClient extends BaseAPIClient {
   fetch(input: RequestInfo, init?: RequestInit): Promise<Response> {
     return globalFetch(input, init);
   }
 }
+
+export const containerStorage: ContainerStorage = {
+  async get(key: string): Promise<string | null> {
+    return globalLocalStorage.getItem(key);
+  },
+  async set(key: string, value: string): Promise<void> {
+    return globalLocalStorage.setItem(key, value);
+  },
+  async del(key: string): Promise<void> {
+    return globalLocalStorage.removeItem(key);
+  },
+};
+
+export const defaultContainer: Container = new Container(
+  "default",
+  new APIClient({
+    apiKey: "",
+    endpoint: "",
+    accessToken: null,
+  }),
+  containerStorage
+);
