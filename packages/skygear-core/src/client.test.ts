1fe0d2bc9640057726c1a129d5b6791f5ff503aa
Louis Chan <louischan@oursky.com>
2019-08-14 16:46:39 +0800

    Handle more edge cases of encodeQuery

--- packages/skygear-core/src/client.test.ts
+++ packages/skygear-core/src/client.test.ts
@@ -2,6 +2,7 @@ import { encodeQueryComponent, encodeQuery } from "./client";
 
 describe("encodeQueryComponent", () => {
   const f = encodeQueryComponent;
+
   it("encode space to +", () => {
     expect(f("a b")).toEqual("a+b");
   });
@@ -9,22 +10,40 @@ describe("encodeQueryComponent", () => {
 
 describe("encodeQuery", () => {
   const f = encodeQuery;
+
   it("encode undefined to empty string", () => {
     expect(f()).toEqual("");
     expect(f(undefined)).toEqual("");
   });
+
   it("encode empty query to empty string", () => {
     expect(f([])).toEqual("");
   });
+
   it("encode query with leading ?", () => {
     expect(f([["a", "b"]])).toEqual("?a=b");
   });
-  it("encode more than one query param", () => {
+
+  it("encode more than one pair", () => {
     expect(f([["a", "b"], ["c", "d"]])).toEqual("?a=b&c=d");
   });
+
   it("encode key and value", () => {
     expect(f([["key1", "a b c"], ["key2", "c d e"]])).toEqual(
       "?key1=a+b+c&key2=c+d+e"
     );
+  });
+
+  it("skip empty pair", () => {
+    expect(f([["", ""], ["a", "b"], ["", ""], ["c", "d"]])).toEqual("?a=b&c=d");
+    expect(f([["a", "b"], ["", ""], ["c", "d"], ["", ""]])).toEqual("?a=b&c=d");
+  });
+
+  it("omit empty value", () => {
+    expect(f([["a", ""], ["b", ""]])).toEqual("?a&b");
+  });
+
+  it("support empty key", () => {
+    expect(f([["", "a"], ["", "b"]])).toEqual("?=a&=b");
   });
 });
--- packages/skygear-core/src/client.ts
+++ packages/skygear-core/src/client.ts
@@ -24,9 +24,14 @@ export function encodeQuery(query?: [string, string][]): string {
   for (let i = 0; i < query.length; ++i) {
     const key = encodeQueryComponent(query[i][0]);
     const value = encodeQueryComponent(query[i][1]);
-    if (i !== 0) {
+
+    if (key === "" && value === "") {
+      continue;
+    }
+    if (output !== "?") {
       output += "&";
     }
+
     output += key;
     if (value !== "") {
       output += "=";
