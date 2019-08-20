82319e476b252025898c8fc492651d452f361d2e
Louis Chan <louischan@oursky.com>
2019-08-20 16:11:57 +0800

    Improve error decoding

--- packages/skygear-core/src/error.test.ts
+++ packages/skygear-core/src/error.test.ts
@@ -17,4 +17,35 @@ describe("decodeError", () => {
     expect(actual.name).toEqual(expected.name);
     expect((actual as any).info).toEqual(expected.info);
   });
+
+  it("returns Error", () => {
+    const e = new Error("test");
+    expect(decodeError(e)).toBe(e);
+  });
+
+  it("decodes object with message", () => {
+    const actual = decodeError({
+      message: "error message",
+    });
+    expect(actual).toBeInstanceOf(Error);
+    expect(actual.message).toEqual("error message");
+  });
+
+  it("decodes object with toString", () => {
+    const actual = decodeError({
+      toString: () => "error message",
+    });
+    expect(actual).toBeInstanceOf(Error);
+    expect(actual.message).toEqual("error message");
+  });
+
+  it("decodes by casting to string", () => {
+    const nullError = decodeError(null);
+    expect(nullError).toBeInstanceOf(Error);
+    expect(nullError.message).toEqual("null");
+
+    const undefinedError = decodeError();
+    expect(undefinedError).toBeInstanceOf(Error);
+    expect(undefinedError.message).toEqual("undefined");
+  });
 });
--- packages/skygear-core/src/error.ts
+++ packages/skygear-core/src/error.ts
@@ -25,13 +25,11 @@ export type SkygearErrorName =
  * @public
  */
 export class SkygearError extends Error {
-  // Ideally it is SkygearErrorName
-  // but the server can emit unrecognized errors.
-  name: string;
   info?: JSONObject;
 
   constructor(message: string, name: string, info?: JSONObject) {
     super(message);
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/name
     this.name = name;
     this.info = info;
   }
@@ -41,9 +39,27 @@ export class SkygearError extends Error {
  * @public
  */
 export function decodeError(err?: any): Error {
-  if (err && typeof err.name === "string" && typeof err.message === "string") {
+  // Construct SkygearError if it looks like one.
+  if (
+    err != null &&
+    !(err instanceof Error) &&
+    typeof err.name === "string" &&
+    typeof err.message === "string"
+  ) {
     return new SkygearError(err.message, err.name, err.info);
   }
-  // TODO: Should we throw a better error?
-  return new Error("unknown error");
+  // If it is an Error, just return it.
+  if (err instanceof Error) {
+    return err;
+  }
+  // If it has message, construct an Error from the message.
+  if (err != null && typeof err.message === "string") {
+    return new Error(err.message);
+  }
+  // If it can be turned into string, use it as message.
+  if (err != null && typeof err.toString === "function") {
+    return new Error(err.toString());
+  }
+  // Otherwise cast it to string and use it as message.
+  return new Error(String(err));
 }
