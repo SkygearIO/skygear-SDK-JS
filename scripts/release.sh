4cb5760ace52f54ddbd7ca4d8792a668a0de68f3
Carmen <carmen@oursky.com>
2019-08-22 19:04:36 +0800

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -29,23 +29,26 @@ else
   exit 1
 fi
 
+npm run set-version "$SKYGEAR_VERSION"
 npm run clean
 npm run format
 npm run lint
 npm run typecheck
 npm run test
 SKYGEAR_VERSION="$SKYGEAR_VERSION" npm run build
+(cd website && yarn && yarn run build)
 
 touch NEWCHANGELOG && cat new-release > NEWCHANGELOG && echo "" >> NEWCHANGELOG && cat CHANGELOG.md >> NEWCHANGELOG && mv NEWCHANGELOG CHANGELOG.md
-git add lerna.json CHANGELOG.md 'packages/*/package.json'
+git add lerna.json CHANGELOG.md 'packages/*/package.json' 'packages/*/package-lock.json' 'website/i18n/*.json'
 git commit -m "Update CHANGELOG for v$SKYGEAR_VERSION"
-git tag -a v"$SKYGEAR_VERSION" -m "Release v$SKYGEAR_VERSION"
+git tag -a v"$SKYGEAR_VERSION" -s -m "Release v$SKYGEAR_VERSION"
 git push git@github.com:SkygearIO/skygear-SDK-JS.git "$GIT_BRANCH"
 git push git@github.com:SkygearIO/skygear-SDK-JS.git v"$SKYGEAR_VERSION"
 
 github-release release -u skygeario -r skygear-SDK-JS --draft --tag v"$SKYGEAR_VERSION" --name v"$SKYGEAR_VERSION" --description "$(cat new-release)"
+rm new-release
 
 (cd packages/skygear-web && npm publish --access public)
 (cd packages/skygear-node-client && npm publish --access public)
 (cd packages/skygear-react-native && npm publish --access public)
-(cd website && yarn && yarn run build && GIT_USER="$GIT_USER" USE_SSH=true yarn run publish-gh-pages)
+(cd website && GIT_USER="$GIT_USER" USE_SSH=true yarn run publish-gh-pages)
