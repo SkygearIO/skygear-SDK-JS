715f957a0d63d312807808190d4f610754e8fdec
Louis Chan <louischan@oursky.com>
2019-09-18 15:52:59 +0800

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,10 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    sourceType: "module",
     project: "./tsconfig.json",
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint",
-  ],
+  plugins: ["@typescript-eslint"],
   env: {
     browser: true,
     node: true,
@@ -17,31 +13,75 @@ module.exports = {
     es6: true,
   },
   rules: {
-    // Turn on some rules that are not included in recommended
+    // picked from recommended
+    // https://github.com/eslint/eslint/blob/master/conf/eslint-recommended.js
+    "constructor-super": "error",
+    "for-direction": "error",
+    "getter-return": "error",
+    "no-async-promise-executor": "error",
+    "no-case-declarations": "error",
+    "no-class-assign": "error",
+    "no-compare-neg-zero": "error",
+    "no-cond-assign": "error",
+    "no-const-assign": "error",
     "no-constant-condition": [
       "error",
       {
         checkLoops: false,
       },
     ],
-    "no-empty": [
+    "no-control-regex": "error",
+    "no-debugger": "error",
+    "no-delete-var": "error",
+    "no-dupe-args": "error",
+    "no-dupe-class-members": "error",
+    "no-dupe-keys": "error",
+    "no-duplicate-case": "error",
+    "no-empty-character-class": "error",
+    "no-empty-pattern": "error",
+    "no-ex-assign": "error",
+    "no-extra-boolean-cast": "error",
+    "no-fallthrough": [
       "error",
       {
-        allowEmptyCatch: true,
+        commentPattern: "fallthrough",
       },
     ],
-    "no-async-promise-executor": "error",
-    "no-await-in-loop": "error",
+    "no-func-assign": "error",
+    "no-global-assign": "error",
+    "no-inner-declarations": "error",
+    "no-invalid-regexp": "error",
     "no-misleading-character-class": "error",
+    "no-new-symbol": "error",
+    "no-obj-calls": "error",
+    "no-octal": "error",
     "no-prototype-builtins": "error",
-    "no-template-curly-in-string": "error",
+    "no-redeclare": "error",
+    "no-regex-spaces": "error",
+    "no-self-assign": "error",
+    "no-shadow-restricted-names": "error",
+    "no-sparse-arrays": "error",
+    "no-this-before-super": "error",
+    "no-undef": "error",
+    "no-unexpected-multiline": "error",
+    "no-unreachable": "error",
+    "no-unsafe-finally": "error",
+    "no-unsafe-negation": "error",
+    "no-unused-labels": "error",
+    "no-useless-catch": "error",
+    "no-useless-escape": "error",
+    "no-with": "error",
     "require-atomic-updates": "error",
+    "use-isnan": "error",
+    "valid-typeof": "error",
+    // not from recommended
+    // TODO: enable this
+    // "no-import-assign": "error",
+    "no-template-curly-in-string": "error",
     complexity: "error",
     "default-case": "error",
     "guard-for-in": "error",
     "no-caller": "error",
-    "no-delete-var": "error",
-    "no-dupe-args": "error",
     "no-else-return": "error",
     "no-eval": "error",
     "no-extend-native": "error",
@@ -56,7 +96,6 @@ module.exports = {
     "no-new": "error",
     "no-new-func": "error",
     "no-new-wrappers": "error",
-    "no-octal": "error",
     "no-proto": "error",
     "no-return-assign": "error",
     "no-return-await": "error",
@@ -67,20 +106,12 @@ module.exports = {
     "no-unmodified-loop-condition": "error",
     "no-unused-expressions": "error",
     "no-useless-call": "error",
-    "no-useless-catch": "error",
     "no-useless-concat": "error",
     "no-useless-return": "error",
     "no-void": "error",
-    "no-with": "error",
     "prefer-promise-reject-errors": "error",
     radix: "error",
-    // We assume the user is using TypeScript with
-    // strictest settings.
-    // An async function without await is not a
-    // problem.
-    "require-await": "off",
     "no-label-var": "error",
-    "no-shadow-restricted-names": "error",
     "no-undef-init": "error",
     "handle-callback-err": "error",
     "no-buffer-constructor": "error",
@@ -91,6 +122,7 @@ module.exports = {
     "no-var": "error",
     "prefer-const": "error",
     "prefer-rest-params": "error",
+    "prefer-spread": "error",
     eqeqeq: [
       "error",
       "always",
@@ -99,19 +131,29 @@ module.exports = {
       },
     ],
     // typescript
-    "@typescript-eslint/camelcase": "off",
-    "@typescript-eslint/class-name-casing": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-member-accessibility": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-extraneous-class": "error",
-    "@typescript-eslint/no-for-in-array": "error",
+    // picked from recommended
+    "@typescript-eslint/adjacent-overload-signatures": "error",
+    // TODO: enable this
+    // "@typescript-eslint/consistent-type-assertions": "error",
+    "@typescript-eslint/no-array-constructor": "error",
+    "@typescript-eslint/no-inferrable-types": [
+      "error",
+      {
+        ignoreParameters: true,
+        ignoreProperties: true,
+      },
+    ],
+    "@typescript-eslint/no-misused-new": "error",
+    "@typescript-eslint/no-namespace": "error",
     "@typescript-eslint/no-this-alias": "error",
-    "@typescript-eslint/no-unnecessary-qualifier": "error",
-    "@typescript-eslint/no-unnecessary-type-assertion": "error",
-    "@typescript-eslint/no-useless-constructor": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "all",
+        ignoreRestSiblings: true,
+        argsIgnorePattern: "^_",
+      },
+    ],
     "@typescript-eslint/no-use-before-define": [
       "error",
       {
@@ -121,13 +163,30 @@ module.exports = {
         typedefs: false,
       },
     ],
-    "@typescript-eslint/no-unused-vars": [
+    "@typescript-eslint/triple-slash-reference": "error",
+    "@typescript-eslint/await-thenable": "error",
+    "@typescript-eslint/no-for-in-array": "error",
+    "@typescript-eslint/no-misused-promises": "error",
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
+    "@typescript-eslint/prefer-regexp-exec": "error",
+    "@typescript-eslint/unbound-method": [
       "error",
       {
-        args: "all",
-        ignoreRestSiblings: true,
-        argsIgnorePattern: "^_",
+        ignoreStatic: true,
       },
     ],
+    // not from recommended
+    "@typescript-eslint/no-extraneous-class": "error",
+    "@typescript-eslint/no-parameter-properties": "error",
+    // TODO: enable this
+    // "@typescript-eslint/no-unnecessary-condition": ["error", {
+    //   ignoreRhs: true,
+    // }],
+    "@typescript-eslint/no-unnecessary-qualifier": "error",
+    "@typescript-eslint/no-useless-constructor": "error",
+    "@typescript-eslint/unified-signatures": "error",
+    "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/strict-boolean-expressions": "off",
   },
 };
