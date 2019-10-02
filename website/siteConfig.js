296459ac135480025b9a5784674c07ebbd6c88dd
Louis Chan <louischan@oursky.com>
2019-10-02 19:25:50 +0800

    Support versioned documentation

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -18,7 +18,7 @@ const siteConfig = {
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [{ doc: "index", label: "API Reference" }],
+  headerLinks: [],
 
   // If you have users set above, you add it here:
 
